cc67d354e801b234af77fb9b6bb3d8d20c7f9047
sachindhanajiingale <130193484+sachindhanajiingale@users.noreply.github.com>
2023-04-27 12:42:30 +0100

    Update forms.py

--- P_app/forms.py
+++ P_app/forms.py
@@ -2,15 +2,27 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm,UsernameField
 from . models import Patient
 
+# class AddPatientForm(forms.ModelForm):
+#     class Meta:
+#         model = Patient
+#         fields =['name','age','phone','Details']
+#         lables = {'name':'Name','age':'Age','phone':'Phone-No'}
+#         widgets ={
+#             'name':forms.TextInput(attrs={'class':'form-control'}),
+#             'age':forms.NumberInput(attrs={'class':'form-control'}),
+#             'phone':forms.TextInput(attrs={'class':'form-control'}),
+#             'Details':forms.TextInput(attrs={'class':'form-control'}),
+#         }
 class AddPatientForm(forms.ModelForm):
     class Meta:
         model = Patient
-        fields =['name','age','phone','Details']
-        lables = {'name':'Name','age':'Age','phone':'Phone-No'}
+        fields =['name','age','phone','visitdate', 'Details']
+        labels = {'name':'Name','age':'Age','phone':'Phone-No', 'date':'visitdate'}
         widgets ={
             'name':forms.TextInput(attrs={'class':'form-control'}),
             'age':forms.NumberInput(attrs={'class':'form-control'}),
             'phone':forms.TextInput(attrs={'class':'form-control'}),
+            'visitdate': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
             'Details':forms.TextInput(attrs={'class':'form-control'}),
         }
 class LogInForm(AuthenticationForm):
