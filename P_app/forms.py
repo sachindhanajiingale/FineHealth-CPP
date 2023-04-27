7911090f282b0fa54ff68adfa50f61ac1ff18d33
sachindhanajiingale <130193484+sachindhanajiingale@users.noreply.github.com>
2023-04-27 15:03:19 +0100

    Update forms.py

--- P_app/forms.py
+++ P_app/forms.py
@@ -23,7 +23,7 @@ class AddPatientForm(forms.ModelForm):
             'name':forms.TextInput(attrs={'class':'form-control'}),
             'age':forms.NumberInput(attrs={'class':'form-control'}),
             'phone':forms.TextInput(attrs={'class':'form-control'}),
-            'visitdate':forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local', 'input_formats': ['%d-%m-%YT%H:%M']}),
+            'visitdate':forms.DateTimeInput(attrs={'class':'form-control', 'type':'datetime-local', 'input_formats':['%Y-%m-%dT%H:%M']}),
             'Details':forms.TextInput(attrs={'class':'form-control'}),
         }
         
